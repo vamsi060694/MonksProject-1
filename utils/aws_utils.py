0467c2e81dfca0b612c514ed10e7d943dfd46e79
sudheer <sudheer.goutham55@gmail.com>
2021-07-18 18:00:18 -0400

    modular

--- utils/aws_utils.py
+++ utils/aws_utils.py
@@ -1,3 +1,9 @@
+import yaml
+import os.path
+import utils.aws_utils as ut
+from pyspark.sql.functions import *
+
+
 
 def get_redshift_jdbc_url(redshift_config: dict):
     host = redshift_config["redshift_conf"]["host"]
@@ -8,8 +14,60 @@ def get_redshift_jdbc_url(redshift_config: dict):
     return "jdbc:redshift://{}:{}/{}?user={}&password={}".format(host, port, database, username, password)
 
 
+#JDBC connection URL for Mysql connection string
 def get_mysql_jdbc_url(mysql_config: dict):
     host = mysql_config["mysql_conf"]["hostname"]
     port = mysql_config["mysql_conf"]["port"]
     database = mysql_config["mysql_conf"]["database"]
     return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
+
+
+
+#Source data loading functions:
+def mysql_SB_data_load(spark,app_conf,app_secret,src_config):
+    jdbc_params = {"url": ut.get_mysql_jdbc_url(app_secret),
+                   "lowerBound": "1",
+                   "upperBound": "100",
+                   "dbtable": src_config["mysql_conf"]["query"],
+                   "numPartitions": "2",
+                   "partitionColumn": src_config["mysql_conf"]["partition_column"],
+                   "user": app_secret["mysql_conf"]["username"],
+                   "password": app_secret["mysql_conf"]["password"]
+                   }
+    txnDF = spark \
+        .read.format("jdbc") \
+        .option("driver", "com.mysql.cj.jdbc.Driver") \
+        .options(**jdbc_params) \
+        .load() \
+        .withColumn('ins_dt', current_date())
+    return txnDF
+
+def sftp_data_load(spark,app_conf,app_secret,src_config):
+        ol_txn_df = spark.read \
+            .format("com.springml.spark.sftp") \
+            .option("host", app_secret["sftp_conf"]["hostname"]) \
+            .option("port", app_secret["sftp_conf"]["port"]) \
+            .option("username", app_secret["sftp_conf"]["username"]) \
+            .option("pem", os.path.abspath(current_dir + "/../../../../" + app_secret["sftp_conf"]["pem"])) \
+            .option("fileType", "csv") \
+            .option("delimiter", "|") \
+            .load(app_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv") \
+            .withColumn('ins_dt', current_date())
+        return ol_txn_df
+
+ def mongo_data_load(spark,app_conf,app_secret,src_config):
+        customer_df = spark \
+            .read \
+            .format("com.mongodb.spark.sql.DefaultSource") \
+            .option("database", app_conf["mongodb_config"]["database"]) \
+            .option("collection", app_conf["mongodb_config"]["collection"]) \
+            .load() \
+            .withColumn('ins_dt', current_date())
+        return customer_df
+
+def s3_data_load(spark,app_conf,app_secret,src_config):
+        campaign_df = spark \
+                .read \
+                .csv("s3://monksworkspace/data/KC_Extract_1_20171009.csv") \
+                .withColumn('ins_dt', current_date())
+        return campaign_df
