1cca7c23394c58a80b394aa92b125107d39f0679
gothmSpark <sugo56gmc!>
2021-07-18 21:09:37 -0700

    changed

--- com.test/source_data_loading.py
+++ com.test/source_data_loading.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
     spark.sparkContext.setLogLevel('ERROR')
 
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir  + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir  + ".secrets")
+    app_config_path = os.path.abspath(current_dir  + "/../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
